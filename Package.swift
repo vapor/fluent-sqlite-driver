43626a1ec892a13d2f6ea89f047002cc25c5fd80
Tanner Nelson <me@tanner.xyz>
2018-02-23 19:43:18 -0500

    update package manifest

--- Package.swift
+++ Package.swift
@@ -2,16 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "Fluent",
+    name: "FluentSQLite",
     products: [
         .library(name: "FluentSQLite", targets: ["FluentSQLite"]),
     ],
     dependencies: [
         // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
         .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
-
-        // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0-rc"),
 
         // ✳️ Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
@@ -23,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/sqlite.git", from: "3.0.0-rc"),
     ],
     targets: [
-        .target(name: "FluentSQLite", dependencies: ["Fluent", "FluentSQL", "Service", "SQLite"]),
+        .target(name: "FluentSQLite", dependencies: ["Async", "Fluent", "FluentSQL", "Service", "SQLite"]),
         .testTarget(name: "FluentSQLiteTests", dependencies: ["FluentBenchmark", "FluentSQLite", "SQLite"]),
     ]
 )

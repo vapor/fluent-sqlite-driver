380f7e9f1afa0090a87c03727a5a3d586f76fb9c
tanner0101 <me@tanner.xyz>
2018-06-19 19:16:23 -0400

    forward group by during query conversion

--- Sources/FluentSQLite/SQLiteDatabase+QuerySupporting.swift
+++ Sources/FluentSQLite/SQLiteDatabase+QuerySupporting.swift
@@ -68,6 +68,7 @@ extension SQLiteDatabase: QuerySupporting {
             select.joins = fluent.joins
             select.predicate = fluent.predicate
             select.orderBy = fluent.orderBy
+            select.groupBy = fluent.groupBy
             select.limit = fluent.limit
             select.offset = fluent.offset
             query = .select(select)

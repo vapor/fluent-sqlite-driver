314185161fcc85cd1df63ad063224141f8b1bf4f
tanner0101 <me@tanner.xyz>
2019-11-18 12:11:05 -0500

    config updates

--- Sources/FluentSQLiteDriver/Exports.swift
+++ Sources/FluentSQLiteDriver/Exports.swift
@@ -3,7 +3,7 @@
 
 // TODO: deprecate
 extension Databases {
-    public func sqlite(
+    public mutating func sqlite(
         configuration: SQLiteConfiguration = .init(storage: .memory),
         maxConnectionsPerEventLoop: Int = 1,
         as id: DatabaseID = .default

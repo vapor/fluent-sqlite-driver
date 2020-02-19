f3c2e81f650653a230039f60603f2b510f0da5be
tanner0101 <me@tanner.xyz>
2020-02-18 19:00:52 -0500

    testcase

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "FluentSQLiteDriver", targets: ["FluentSQLiteDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("tn-testcase")),
         .package(url: "https://github.com/vapor/sqlite-kit.git", from: "4.0.0-beta.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],

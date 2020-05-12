8ffbe45bb9e7a774a766cf10f9c510b266127cb8
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-05-12 14:56:42 -0500

    Use prepared-migration-filtering FluentKit branch

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "FluentSQLiteDriver", targets: ["FluentSQLiteDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-rc.1"),
+        .package(url: "https://github.com/vapor/fluent-kit.git", .branch("prepared-migration-filtering")),
         .package(url: "https://github.com/vapor/sqlite-kit.git", from: "4.0.0-rc.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],

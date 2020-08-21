ed24c3048b6fc8f0e091fc6391006fb6707d2b83
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-08-21 14:34:07 -0500

    Minimum iOS version is 13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "fluent-sqlite-driver",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         .library(name: "FluentSQLiteDriver", targets: ["FluentSQLiteDriver"]),

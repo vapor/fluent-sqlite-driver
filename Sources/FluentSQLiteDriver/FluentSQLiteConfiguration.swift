9ca34be792979fb0f1dbd8e45b8af9f1e1440474
Mads Odgaard <mads@madsodgaard.com>
2021-06-04 17:52:58 +0200

    Add configurable connection pool timeout (#74)
    * add connection pool timeout option
    
    * update ci

--- Sources/FluentSQLiteDriver/FluentSQLiteConfiguration.swift
+++ Sources/FluentSQLiteDriver/FluentSQLiteConfiguration.swift
@@ -1,13 +1,17 @@
+import NIO
+
 extension DatabaseConfigurationFactory {
     public static func sqlite(
         _ configuration: SQLiteConfiguration = .init(storage: .memory),
-        maxConnectionsPerEventLoop: Int = 1
+        maxConnectionsPerEventLoop: Int = 1,
+        connectionPoolTimeout: NIO.TimeAmount = .seconds(10)
     ) -> Self {
         return .init {
             FluentSQLiteConfiguration(
                 configuration: configuration,
                 maxConnectionsPerEventLoop: maxConnectionsPerEventLoop,
-                middleware: []
+                middleware: [],
+                connectionPoolTimeout: connectionPoolTimeout
             )
         }
     }
@@ -17,6 +21,7 @@ struct FluentSQLiteConfiguration: DatabaseConfiguration {
     let configuration: SQLiteConfiguration
     let maxConnectionsPerEventLoop: Int
     var middleware: [AnyModelMiddleware]
+    let connectionPoolTimeout: NIO.TimeAmount
 
     func makeDriver(for databases: Databases) -> DatabaseDriver {
         let db = SQLiteConnectionSource(
@@ -26,6 +31,7 @@ struct FluentSQLiteConfiguration: DatabaseConfiguration {
         let pool = EventLoopGroupConnectionPool(
             source: db,
             maxConnectionsPerEventLoop: maxConnectionsPerEventLoop,
+            requestTimeout: connectionPoolTimeout,
             on: databases.eventLoopGroup
         )
         return _FluentSQLiteDriver(pool: pool)

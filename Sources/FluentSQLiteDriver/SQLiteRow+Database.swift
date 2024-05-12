3b2e038d7ebc00bceb39912cc85c26f68ede2937
Gwynne Raskind <gwynne@vapor.codes>
2024-05-12 12:19:56 -0500

    Fix SQliteDatabaseOutput

--- Sources/FluentSQLiteDriver/SQLiteRow+Database.swift
+++ Sources/FluentSQLiteDriver/SQLiteRow+Database.swift
@@ -20,21 +20,25 @@ private struct SQLiteDatabaseOutput: DatabaseOutput {
     
     /// The most recently set schema value (see `DatabaseOutput.schema(_:)`).
     let schema: String?
+    
+    private func column(for key: FieldKey) -> String {
+        (self.schema.map { FieldKey.prefix(.prefix(.string($0), "_"), key) } ?? key).description
+    }
 
     func schema(_ schema: String) -> DatabaseOutput {
         SQLiteDatabaseOutput(row: self.row, schema: schema)
     }
 
     func contains(_ key: FieldKey) -> Bool {
-        self.row.contains(column: key.description)
+        self.row.contains(column: self.column(for: key))
     }
 
     func decodeNil(_ key: FieldKey) throws -> Bool {
-        try self.row.decodeNil(column: key.description)
+        try self.row.decodeNil(column: self.column(for: key))
     }
 
     func decode<T: Decodable>(_ key: FieldKey, as type: T.Type) throws -> T {
-        try self.row.decode(column: key.description, as: T.self)
+        try self.row.decode(column: self.column(for: key), as: T.self)
     }
     
     var description: String { "" }
